6d5e55b5a0c34e8af1afe9d54a5278755f533e0e
bubbaman <renee.suman@proton.me>
2026-04-03 20:40:30 +0200

    chore: sync 2026-04-03 20:40

--- mio_master/urls.py
+++ mio_master/urls.py
@@ -14,8 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
@@ -52,6 +50,3 @@ urlpatterns = [
     path('link_storage/', include('link_storage.urls')),
     path('contacts/', include('contacts.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
